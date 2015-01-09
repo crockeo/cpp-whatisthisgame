2d99679b5963b9e06fd9a911fbce1fa50148c855
Cerek Hillen <crockeo@gmail.com>
2015-01-08 22:54:29 -0500

    Got a window opening.

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,29 @@
+//////////////
+// Includes //
+#include <GLFW/glfw3.h>
 #include <iostream>
-int main() { std::cout << "Hello world!" << std::endl; }
+
+//////////
+// Code //
+
+// The entry point to the application.
+int main() {
+    if (!glfwInit()) {
+        std::cout << "Could not start GLFW." << std::endl;
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello world", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Could not open the GLFW window." << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
