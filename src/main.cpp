a8b5713bcf96bea410c0a7d2ad520b7e8ee15df9
Cerek Hillen <crockeo@gmail.com>
2015-01-10 01:44:45 -0500

    Set the OpenGL version to be 3.3

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,11 @@ bool startEngine(Config cfg) {
     }
 
     // Creating the window.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window = glfwCreateWindow(
         cfg.windowWidth,
         cfg.windowHeight,
