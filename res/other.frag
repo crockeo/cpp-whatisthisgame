e14f33dfe98c4aef966b28f8dcc14692aa70b077
Cerek Hillen <crockeo@gmail.com>
2015-01-12 00:48:16 -0500

    Got a texture rendering. But it's all screwy.

--- res/other.frag
+++ res/other.frag
@@ -1,10 +1,13 @@
 #version 150
 
+uniform sampler2D tex;
+
 in vec3 Color;
+in vec2 TexCoord;
 
 out vec4 outColor;
 
 void main()
 {
-    outColor = vec4(Color, 1.0);
+    outColor = texture(tex, TexCoord) * vec4(Color, 1);
 }
--- src/rendering.cpp
+++ src/rendering.cpp
@@ -28,10 +28,10 @@ void rendering::renderVertices(std::vector<std::tuple<float, float>> vs, Texture
     GLuint vbo;
     glGenBuffers(1, &vbo);
     GLfloat vertices[] = {
-        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
     };
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -51,11 +51,19 @@ void rendering::renderVertices(std::vector<std::tuple<float, float>> vs, Texture
     glUseProgram(s.getID());
     GLint posAttrib = glGetAttribLocation(s.getID(), "position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
 
     GLint colAttrib = glGetAttribLocation(s.getID(), "color");
     glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+    GLint texAttrib = glGetAttribLocation(s.getID(), "texCoord");
+    glEnableVertexAttribArray(texAttrib);
+    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, t.getID());
+    glUniform1i(glGetUniformLocation(s.getID(), "texSampler"), 0);
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
