32eb12218b78a73611aef718b8754fbba63b2a73
Cerek Hillen <crockeo@gmail.com>
2015-01-13 17:10:52 -0500

    Framework of the Render thing

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -3,6 +3,48 @@
 //////////
 // Code //
 
+// Constructing a single render for a set of vertices, a Texture, and a
+// sprite.
+Render::Render(std::vector<std::tuple<float, float>> coords,
+               std::vector<std::tuple<float, float>> texCoords,
+               Texture texture, Shader shader, GLenum type) :
+        texture(texture),
+        shader(shader) {
+
+
+    this->updateVertices(coords, texCoords);
+}
+
+// Cleaning up this render.
+Render::~Render() {
+    // TODO: FREEE FALLING
+}
+
+// Updating the set of vertices in this render.
+void Render::updateVertices(std::vector<std::tuple<float, float>> coords,
+                            std::vector<std::tuple<float, float>> texCoords) {
+    // TODO: Should I throw an error from this?
+    if (coords.size() != texCoords.size())
+        return;
+
+    // TODO: GET STUFF DONE.
+}
+
+// Binding this Render.
+void Render::bind() const {
+    // TODO: GET STUFF DONE.
+}
+
+// Unbinding this Render.
+void Render::unbind() const {
+    // TODO: GET STUFF DONE.
+}
+
+// Rendering this Render.
+void Render::render() const {
+    // TODO: GET STUFF DONE.
+}
+
 // Making the constructor private by default.
 Renderer::Renderer() { }
 
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -4,10 +4,47 @@
 //////////////
 // Includes //
 #include <vector>
+#include <tuple>
+
+#include "res/texture.hpp"
+#include "res/shader.hpp"
 
 //////////
 // Code //
 
+// A class to represent a single texture render.
+class Render {
+private:
+    GLuint vao;
+    GLuint vbo;
+    GLuint ebo;
+    GLfloat* vertices;
+    Texture texture;
+    Shader shader;
+
+public:
+    // Deleting the copy constructor.
+    Render(const Render&) = delete;
+
+    // Constructing a single render for a set of vertices, a Texture, and a
+    // sprite.
+    Render(std::vector<std::tuple<float, float>>, std::vector<std::tuple<float, float>>, Texture, Shader, GLenum);
+
+    // Cleaning up this render.
+    ~Render();
+
+    // Updating the set of std::vector<std::tuple<float, float>> in this render.
+    void updateVertices(std::vector<std::tuple<float, float>>, std::vector<std::tuple<float, float>>);
+
+    // Binding this Render.
+    void bind() const;
+
+    // Unbinding this Render.
+    void unbind() const;
+
+    // Rendering this Render.
+    void render() const;
+};
 
 // A class to encapsulate rendering.
 class Renderer {
